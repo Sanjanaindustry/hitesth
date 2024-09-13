fb1cfb13dd3b7f122f984d126e0e2c4dd0c97104
Sanjanaindustry <130057644+Sanjanaindustry@users.noreply.github.com>
2024-09-13 11:07:02 +0000

    Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict";//treat all js code as newer version
+
+//alert(3+3)//we are using nodejs not browser
+console.log(3 +3)// code readability should be high
+console.log("hitesh")
+
+let name="hitesh"
+let age=18
+let isLoggedIn=false
+
+//number =>2 to power 53
+//bigint
+//string
+
+console.log(typeof "hitesh");
