77e2e1e89d72e35d76ec199af88bc6553ef9d945
Sanjanaindustry <130057644+Sanjanaindustry@users.noreply.github.com>
2024-09-13 10:38:02 +0000

    config is done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId="12345"
+var accountPassword="12345"
+let accountEmail="hitest@google.com"
+accountCity="jaipur"
+
+// accountId = 2 //
+accountEmail="sanju@google.com"
+accountPassword="22225"
+accountCity="bengaluru"
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword])
