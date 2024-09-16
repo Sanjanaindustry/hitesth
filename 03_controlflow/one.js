8205ff621f87568218740c7e889aef9b148ea74d
Sanjanaindustry <130057644+Sanjanaindustry@users.noreply.github.com>
2024-09-16 13:07:17 +0000

    ifelse and switch

---
+++ 03_controlflow/one.js
@@ -0,0 +1,79 @@
+
+//node 03_controlflow/one.js
+//if
+// const temperature="41"
+// if(temperature<22){
+//     console.log("less than 50");
+
+// }
+// else{
+//     console.log("temp is greater than 50");
+// }
+
+// const score=200
+// if(score>100){
+//     const power="fly"
+// console.log(`user pwoer:${power}`)
+// };
+// console.log(`user pwoer:${power}`)
+// var can be accessed gloablly but not the
+
+const balance=1000
+
+// if(balance>500) console.log("test");
+// //dont use , and use more implicitly
+
+// if(balance<500){
+//     console.log("less than")
+
+// }
+// else if(balance<750){
+//     console.log("less than 500")
+
+// }
+// else{
+//     console.log("is greater than 500 ")
+// }
+// const userloggedin=true
+// const debitcard=true
+// const clientloggin=false
+// const scamloggin=true
+// const googleloggin=false
+// if(userloggedin && debitcard && 2==2){
+//     console.log("allow to nuy course");
+// }
+// if(clientloggin || scamloggin){
+//     console.log("user logged in");
+// }
+   
+
+
+
+//****************SWITCH ***********************
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+ const month="amrch"
+
+ switch (month) {
+    case "jan":
+        console.log("jan")
+        break;
+ 
+    case 4:
+        console.log("april")
+        break;
+
+    case 3:
+        console.log("march")
+        break;
+ 
+    default:
+        console.log("hehe")
+        break;
+ }
