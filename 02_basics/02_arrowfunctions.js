cc4c8d69beee1542647672a01804e96d2f1f2a16
Sanjanaindustry <130057644+Sanjanaindustry@users.noreply.github.com>
2024-09-16 12:22:47 +0000

    arrow functions

---
+++ 02_basics/02_arrowfunctions.js
@@ -0,0 +1,51 @@
+// const user1={
+//     name:"sanju",
+//     age:30,
+    
+// }
+// user1.welcomemessage=function() { 
+//     console.log(`${this.name}`);
+//     }
+//     console.log(user1.welcomemessage());
+
+//  function chai(){
+//      username="hii"
+//     console.log(`${this.username}`);
+//  }   
+//  chai()
+//  function chai(){
+//      username="hii"
+//     console.log(`${this.username}`);
+//  }   
+//  chai()
+//  const chai =function(){ execute agthe edu
+//      username="hii"
+//     console.log(`${this.username}`);
+//  }   
+//  chai()
+
+
+
+
+
+//arrow functions
+//  const chai =()  =>{
+//     username="hii"
+//    console.log(this);
+// }   
+// chai()
+
+
+//interesting//
+// const addtwo=(num1,num2)=>{
+//     return nm1+num2
+// }
+// const addone=(num1,num2)=>{
+//     return num1+num2
+// }
+// console.log(addone(8,9));
+
+// const addone=(num1,num2)=>  (num1+num2)
+v
+console.log(addone(8,9));
+
