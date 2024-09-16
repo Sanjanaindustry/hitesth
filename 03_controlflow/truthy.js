e37cf3c5a7bccaa980ab7d3bc92e701014f85249
Sanjanaindustry <130057644+Sanjanaindustry@users.noreply.github.com>
2024-09-16 14:03:48 +0000

    truth

---
+++ 03_controlflow/truthy.js
@@ -0,0 +1,32 @@
+
+// node 03_controlflow/truthy.js
+
+// const useremail=[]
+// if(useremail){
+//     console.log("got email");
+// }
+// else{
+//     console.log("dont jhave");
+// }
+
+// if(useremail.length===0){
+//     console.log(" arrayis emply")// array emptinesss
+// }
+
+// const emptyobj={}
+// if(Object.keys(emptyobj).length===0){ //to check object emptiness
+//  console.log("is empty")
+// }
+
+//****NULLISHHHHHH */
+// let val1;
+// // val1=9 ?? 8
+// // val1=null ?? 10
+// val1=undefined ?? 16
+// console.log(val1);
+
+
+//****terinary */
+
+const icetea=100
+icetea>=80 ? console.log("greater than 80 ") :console.log("less than 80")
