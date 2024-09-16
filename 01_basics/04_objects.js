39605b9a40f8090eef4e78b3ec2bbb8821a51ea2
Sanjanaindustry <130057644+Sanjanaindustry@users.noreply.github.com>
2024-09-16 12:22:30 +0000

    objects

--- 01_basics/04_objects.js
+++ 01_basics/04_objects.js
@@ -3,35 +3,35 @@
 
 // //thisliterals
 
-// const sujan={
-//     name:"sanj",
-//     age:20,
-//     location:"davangere",
-//     email:"sanjana@.com",
-//     isloggedIn:false,
-//     lastlogindays:["mon","tue"]
-// }
-
-// // console.log(sujan["email"]);
-// // sujan.email=""//
-
-// sujan.greeting=function(){
-//     console.log("hello js ser");
-// }
-// sujan.greetingtwo=function(){
-//     console.log(`hello js ser,${this.name}`);
-// }
-// console.log(sujan.greeting());
-// console.log(sujan.greetingtwo());
-
-const course={
-    coname:"hhh",
-    coursefee:88,
-    courseinstructor:"ramu"
+const sujan={
+    name:"sanj",
+    age:20,
+    location:"davangere",
+    email:"sanjana@.com",
+    isloggedIn:false,
+    lastlogindays:["mon","tue"]
 }
-const{courseinstructor :ci}=course
-console.log(ci);
-{
-    name:"sanj"
-    id:"sss"
+
+// console.log(sujan["email"]);
+// sujan.email=""//
+
+sujan.greeting=function(){
+    console.log("hello js ser");
 }
+sujan.greetingtwo=function(){
+    console.log(`hello js ser,${this.name}`);
+}
+console.log(sujan.greeting());
+console.log(sujan.greetingtwo());
+
+// const course={
+//     coname:"hhh",
+//     coursefee:88,
+//     courseinstructor:"ramu"
+// }
+// const{courseinstructor :ci}=course
+// console.log(ci);
+// {
+//     name:"sanj"
+//     id:"sss"
+// }
