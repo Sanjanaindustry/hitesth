cca62403948904f6a82f6db346f9472131ce9e69
Sanjanaindustry <130057644+Sanjanaindustry@users.noreply.github.com>
2024-09-16 12:21:56 +0000

    functions

---
+++ 01_basics/05_functions.js
@@ -0,0 +1,94 @@
+
+ 
+// // function saymyname(){
+// //     console.log("H");
+// // console.log("T");
+// // console.log("J");
+// // console.log("O");
+// // console.log("P");
+
+// // }
+// // //saymyname()
+// // // function addtwonumbers(num1 ,num2){
+// // //    // num1+num2//doresnt print so
+// // //    console.log( num1+num2)
+// // // }
+
+// // // function addtwonumbers(num1 ,num2){
+// // //     // let result=num1+num2
+// // //     return result
+// // //     return num1+num2
+// // //  }
+// // // const result=addtwonumbers(2,3)
+// // // console.log("res:", result);
+
+// // //how many ways argument can be passed//
+// // function login(username){
+// //     if(username===undefined){
+// //         console.log("please enter the username");
+// //         return
+// //     }
+// //     return `${username} just loggedin`
+// // }
+// // // console.log(login("sanju"));
+// // console.log(login());
+
+// // function calcartprice(...num1){
+// //     return num1
+// // }
+// // console.log(calcartprice(,3,5,2));
+
+// const user={
+//     username:"kkk",
+//     price:99
+// }
+// function handleobject(anyobject){
+//     console.log(`username is ${anyobject.username} and price is ${anyobject.price}`)
+// }
+// //handleobject(user) direct below  method
+// handleobject({
+//     username:"sam",
+//     price:0
+// })
+
+// //array functo//
+
+// const myarray=[200,200,400,88]
+//  function ramdev(getarray){
+//     return getarray[3]
+//  }
+//  //console.log(ramdev(myarray));
+//  console.log(ramdev([200,200,400,88]));
+
+//  var tej="iii"
+//  function raju(){
+//     // var tej="ioi"
+//     // return tej
+//     // console.log((tej));
+//  }
+//  console.log((tej));
+
+// const username="sanjudon"
+// if(true){
+//     const username="hitesh"
+//     if(username=="hitesh"){
+//         const web="youtube"
+//         console.log(username+web);
+//     }
+    
+// }
+// console.log(username);
+
+
+//****************interesting *//
+function addone(num){
+    return num+1
+}
+addone(4)
+
+const addtwo=function(num){
+    return num+2
+}
+addtwo(7)
+
+
