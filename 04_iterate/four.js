dc2a71bf62b6a51fe369cea4b8f290997ed8414b
Sanjanaindustry <130057644+Sanjanaindustry@users.noreply.github.com>
2024-09-17 13:28:43 +0000

    filter

---
+++ 04_iterate/four.js
@@ -0,0 +1,17 @@
+
+
+// node 04_iterate/four.js
+// const mynums=[1,2,3,4,5,6,7]
+// mynums.filter( (num)=> {
+//    return num>4
+// })
+
+// const newnums=mynums.filter((num)=>num>4)
+// console.log(newnums);
+
+// const newnums=[]
+// newnums.forEach((num)=>{
+//     if(num>4)
+//         {
+//             newnums.push(num)
+//         }
