7ed1c2f72be65d26704985b5904c4f04cd966994
Sanjanaindustry <130057644+Sanjanaindustry@users.noreply.github.com>
2024-09-17 09:12:27 +0000

    do while

---
+++ 04_iterate/two.js
@@ -0,0 +1,21 @@
+
+// let i=0
+// while (i<=10) {
+//     console.log(` the vale is ${i}`);
+//     i=i+2
+// }
+
+// let array=["arr","ram","sham"]
+// let i=0
+// while (i<array.length) {
+//     console.log(`${array[i]}`);
+//    i=i+1
+    
+// }
+
+//do while
+let score=9
+do {
+    score=score*2
+    console.log(`${score}`);
+} while (score<=9);
