c438e78059cef010991f7dbd9f577c061ccc9b25
Sanjanaindustry <130057644+Sanjanaindustry@users.noreply.github.com>
2024-09-17 11:24:25 +0000

    foreach

---
+++ 04_iterate/three.js
@@ -0,0 +1,82 @@
+//node 04_iterate/three.js
+// 1
+
+// const i=[1,2,3,4]
+// for (const val of i) {
+//     console.log(val);
+    
+// }
+
+// const greeting="hellow"
+// for(const i of greeting){
+//     console.log(`each char is ${i}`);
+// }
+
+
+//maps
+// const map=new Map()
+// map.set("IN","india")
+// map.set("ranj","sam")
+// map.set("ronny","roxk")
+// console.log(map);
+
+
+// for (const [key ,value] of map) {
+//     // console.log(key,':-',value);
+    
+// }
+//  const myobj={
+//     'game1':'rockers',
+//     'game2':'spiderman'
+//  }
+
+//  for (const [key, value] of myobj) {
+//     console.log(key, value);
+    
+//  }
+//  const oob={
+//     js:'javascirpt',
+//     kk:"jjjj"
+//  }
+
+// //  for (const ram in oob) {
+// //     console.log(`{ram} fro is ${oob[ram]}`);
+  
+// //  }
+
+
+// const coding=["kk","iii","lolo"]
+// // coding.forEach(function (item) {
+// //     console.log(item);
+// // }    )
+
+// // coding.forEach( (item)=>{
+// //     console.log(item)
+// // })
+
+
+
+// function printme(item,index,arr){
+//     console.log(item,index,arr);
+// }
+// coding.forEach(printme)
+
+
+// const mycoding =[
+//     {
+//         uu:"ppp",
+//         ooo:"kkk"
+//     },
+//     {
+//         upN:"gly",
+//         boc:"cutie"
+//     },
+//     {
+//         ii:"ram",
+//         metro:"rani"
+//     }
+// ]
+// mycoding.forEach( (item) => {
+//     console.log(item.boc);
+
+// })
